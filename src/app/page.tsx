91187e1a7a8a76331b36d39f221e00c629362756
Rana M Awais <ranamawais92@gmail.com>
2024-02-19 23:18:55 +0500

    image tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+import piaic from '../../public/lap.jpg'
 
 export default function Home() {
   return (
-    <main className="font-bold px-4 py-4 ">
+    <main className="font-bold px-4 py-4">
       <div>Hello World</div>
       <p>
         This is a vercel project
       </p>
+      <div>
+
+        <Image src={piaic}
+        width={400}
+        height={200}
+        alt="Image"
+        className="border-4 rounded-full border-orange-500">
+
+        </Image>
+      </div>
     </main>
   );
 }
