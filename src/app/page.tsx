2dffcbf828498a495a646959144551877c9ae90a
Rana M Awais <ranamawais92@gmail.com>
2024-02-19 19:33:08 +0500

    background-cahnge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="font-bold px-4 py-4 ">
       <div>Hello World</div>
+      <p>
+        This is a vercel project
+      </p>
     </main>
   );
 }
